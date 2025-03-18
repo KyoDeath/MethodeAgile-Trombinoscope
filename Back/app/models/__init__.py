09036f4501e983b0778c573335d649f66afb42cc
KyoDeath <ethangenevriez@gmail.com>
2025-03-18 16:24:28 +0100

    Config Init Models

---
+++ Back/app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.db.database import Base
+from app.models.role import Role
+from app.models.user import User
+from app.models.classe import Classe
+from app.models.user_classes import UserClasses
